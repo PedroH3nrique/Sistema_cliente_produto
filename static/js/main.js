4d9cbb6575b7d17efc4693048bd6c3a682398479
PedroH3nrique <pedrohenriquerod.santos@gmail.com>
2025-04-20 14:18:21 -0300

    Adicionando função de busca de clientes e melhorando o design

--- static/js/main.js
+++ static/js/main.js
@@ -1,8 +1,16 @@
-function buscarClt(){
-    const inpt = document.getElementById('buscarClt')
-
-    if(inpt.value.length > 0){
-        alert(inpt.value)
+function buscarClt() {
+    const input = document.getElementById('buscarClt');
+    const filter = input.value.toUpperCase();
+    const rows2 = document.getElementById('corpoTabela').getElementsByTagName('tr');
+    
+    for (let i = 0; i < rows2.length; i++) {
+        const cells = rows2[i].getElementsByTagName('td');
+        if (cells.length > 0) {
+            const nameCell = cells[1]; // Ajuste para a coluna "Nome"
+            if (nameCell) {
+                const txtValue = nameCell.textContent || nameCell.innerText;
+                rows2[i].style.display = txtValue.toUpperCase().indexOf(filter) > -1 ? '' : 'none';
+            }
+        }
     }
-
 }
